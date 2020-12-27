d2b0a23ff49f5f218ae964d89276f600bd30138b
Bharathi Ratna <52811537+cse-stud@users.noreply.github.com>
2020-12-27 18:31:31 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ import code as x
 #importing the main file("code" is the name of the file I have used) as a library 
 
 
-x.create("sastra",25)
+x.create("pvpsit",25)
 #to create a key with key_name,value given and with no time-to-live property
 
 
@@ -15,7 +15,7 @@ x.create("src",70,3600)
 #to create a key with key_name,value given and with time-to-live property value given(number of seconds)
 
 
-x.read("sastra")
+x.read("pvpsit")
 #it returns the value of the respective key in Jasonobject format 'key_name:value'
 
 
@@ -23,18 +23,18 @@ x.read("src")
 #it returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
 
 
-x.create("sastra",50)
+x.create("pvpsit",50)
 #it returns an ERROR since the key_name already exists in the database
 #To overcome this error 
 #either use modify operation to change the value of a key
 #or use delete operation and recreate it
 
 
-x.modify("sastra",55)
+x.modify("pvpsit",55)
 #it replaces the initial value of the respective key with new value 
 
  
-x.delete("sastra")
+x.delete("pvpsit")
 #it deletes the respective key and its value from the database(memory is also freed)
 
 #we can access these using multiple threads like
